8eef7f1684676669d830b20f2ed05adb327072ce
Sisamgtm07 <sisamgautam0001@gmail.com>
2021-03-14 19:33:28 +0545

    Task2 Completed

--- Day 3/task2.php
+++ Day 3/task2.php
@@ -1,4 +1,19 @@
 <?php
 //write a program to print your name and comment about you
+print "My name is Sisam Gautam.";
+
+
+/* ========================================================================================================
+
+
+I am  student at Nepathya College pursuing my undergraduate degree in CSIT.
+I am web enthusiast who loves to explore new things, and keep herself updated with the latest in technologies.
+Apart from my career based interest, I discovered my interest in graphics designing too.
+I am a diligent student,who is passionate about growing teams and dreams.
+I believe that personal development is one of the supreme qualities an individual should build.
+With stronghold supervision,currently,she is involving in different tech association: CSITAN-Rupandehi, Nepathya Student’s Council and Butwal Coder Girls.
+
+
+===========================================================================================================*/
 
 ?>
