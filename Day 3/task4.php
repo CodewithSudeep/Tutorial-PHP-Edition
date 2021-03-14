a492eae50ac569d3115df6df1d3618244b1953c3
Sisamgtm07 <sisamgautam0001@gmail.com>
2021-03-14 19:48:12 +0545

    Task4 Completed

--- Day 3/task4.php
+++ Day 3/task4.php
@@ -1,3 +1,10 @@
 <?php 
 
 //write a program to demonstrate the use of multi line comments
+
+/*=================================================================
+Multiline comments are used for large text descriptions of code.
+This is a multiple-lines comment block that spans over multiple lines.
+====================================================================*/
+
+?>
