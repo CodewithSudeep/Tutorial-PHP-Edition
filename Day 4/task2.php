e1430660eef8548eba82273e94e4c877e24742a8
rikesh-a11 <rikeshshrestha499@gmail.com>
2021-03-15 19:13:53 +0545

    task2

--- Day 4/task2.php
+++ Day 4/task2.php
@@ -1,3 +1,14 @@
 <?php
+$name ="rikesh";  #string
+$roll=21;  #int
+$marks=80.5;  #float
+
+#printing
+echo "$name";
+
+echo "$roll";
+
+echo "$marks";
+
 
 //create variables for : string data type, int, float.
