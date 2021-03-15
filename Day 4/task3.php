abd49b33174e92194ec67784629971e29bc0b602
Bibek_16 <71329649+Bibek117@users.noreply.github.com>
2021-03-15 19:38:24 +0545

    day4 task fully done

--- Day 4/task3.php
+++ Day 4/task3.php
@@ -1,3 +1,6 @@
 <?php
 
 //create and print a value of variable having your name as data.
+$Name = "Bibek Angdembe";
+echo $Name;
+?>
--- Day 4/task4.php
+++ Day 4/task4.php
@@ -1,3 +1,6 @@
 <?php
-
 // create two variables for string data type and print then using h1 HTML tag.
+$text1 = "Welcome to ";
+$text2 = "CodewithSudeep";
+echo "<h1>$text1</h1>","<h1>$text2</h1>";
+?>
