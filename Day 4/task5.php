e55827a73b7679ad30277f7d836f7aca39c8752f
Sisamgtm07 <sisamgautam0001@gmail.com>
2021-03-15 19:41:54 +0545

    All Tasks Completed

--- Day 4/task5.php
+++ Day 4/task5.php
@@ -3,3 +3,4 @@
 // create a varibale and store basic syntax of php in it. print the variable using echo and <code> tag of HTML
 $var1 = 'echo "Hello World".';
 echo "Syntax for echo: <code> $var1 </code> ";
+?>
