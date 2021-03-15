27e682aab749fe26a0bc0caf7e908ceb39169e80
rikesh-a11 <rikeshshrestha499@gmail.com>
2021-03-15 19:45:38 +0545

    task4

--- Day 4/task4.php
+++ Day 4/task4.php
@@ -1,3 +1,25 @@
-<?php
 
-// create two variables for string data type and print then using h1 HTML tag.
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>bio</title>
+</head>
+<body>
+
+<h1>
+<?php
+$name="rik";
+$name1="rikesh";
+
+echo "$name";
+echo "<br/>";
+echo "$name1";
+?>
+</h1>
+</body>
+</html>
+
