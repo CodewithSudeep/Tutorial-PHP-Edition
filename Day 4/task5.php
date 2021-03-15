f94e1663c2571eaad8222a6347f867c3264e968c
Sisamgtm07 <sisamgautam0001@gmail.com>
2021-03-15 20:10:37 +0545

    Changes on task5

--- Day 4/task5.php
+++ Day 4/task5.php
@@ -1,6 +1,6 @@
 <?php
 
 // create a varibale and store basic syntax of php in it. print the variable using echo and <code> tag of HTML
-$var1 = 'echo "Hello World".';
-echo "Syntax for echo: <code> $var1 </code> ";
+$var1 = "< ? php //..........code.........  ? >";
+echo "Basic Syntax for php is: <br> <code> $var1 </code> ";
 ?>
