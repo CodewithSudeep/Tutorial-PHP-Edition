368f101d8c4d4bf94d988b5745f0f71d185ee2ab
sakshamkadayat <sakshamkadayat@gmail.com>
2021-03-16 17:02:40 +0000

    task completed

--- Day 2/task3.php
+++ Day 2/task3.php
@@ -1,1 +1,7 @@
-display your name using echo statement 
+display your name using echo statement
+
+<?php
+
+echo "My name is saksham";
+
+?>
--- Day 3/task2.php
+++ Day 3/task2.php
@@ -1,4 +1,11 @@
 <?php
 //write a program to print your name and comment about you
 
+
+echo " hello i am saksham kadayat "; 
+
+/*
+Hello  This is comment section created by saks.
+*/ 
+
 ?>
