8bbab9fed834d755296f8c85eebb72aa91b21aef
rikesh-a11 <rikeshshrestha499@gmail.com>
2021-03-19 20:49:38 +0545

    task2

--- Day 8/task2.php
+++ Day 8/task2.php
@@ -1,4 +1,12 @@
 <?php
 
-//use arithmetic operator to subtract two float numbers
-//perform same task using assignment operator
+$n1=12.5;
+$n2=10.2;
+
+$sub=$n1-$n2;
+echo "the substract is $sub <br/>";   #using arithmetic operator
+
+$n1-=$n2;
+echo "the substract is $n1 <br/>";  #using the adiitional  assignment operator
+?>
+
