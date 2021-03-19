1e0a3f8495d6a2933535f94d0aa1a72f9741e146
rikesh-a11 <rikeshshrestha499@gmail.com>
2021-03-19 20:52:47 +0545

    task3

--- Day 8/task3.php
+++ Day 8/task3.php
@@ -1,4 +1,11 @@
 <?php
 
-//use arithmetic operator to multiply two int
-//perform same task using assignment operator
+$n1=12;
+$n2=10;
+$mul=$n1*$n2;
+echo "the product is $mul<br/>";  #arithmetic opertaors
+
+$n1*=$n2;
+echo "the priduct is $n1<br/>";
+?>
+
