5f395df2f66eb13e40681aee560a12eded1efbcb
Sisamgtm07 <sisamgautam0001@gmail.com>
2021-03-20 18:14:38 +0545

    Task 8 Completed

--- Day 6/task8.php
+++ Day 6/task8.php
@@ -1,3 +1,10 @@
 <?php
 
 //1. reverse the varibale created in task7 and find the position of your college name in string.
+$var1 = "Hello, I am Sisam Gautam.";
+$replace = str_replace("Sisam Gautam", "Nepathya College", $var1);
+echo $replace . "<br>";
+echo strrev($var1) . "<br>";
+$pos = strpos($replace, "Nepathya College");
+echo "The position of my college name is:", $pos;
+?>
