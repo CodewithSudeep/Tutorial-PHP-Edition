912be0f6cae9419cfbc092eec9553fb7cc55a307
PradipKun <pradipunwar333@gmail.com>
2021-03-22 06:45:17 +0545

    task corrected

--- Day 8/task8.php
+++ Day 8/task8.php
@@ -3,8 +3,8 @@
 //find the value of smallest prime number to the power highest prime number in range 50-100
 $prime_low=53;
 $prime_high=97;
-$prod=$prime_high*$prime_low;
+$prod=$prime_high**$prime_low;
 echo "</br>";
-echo "($prime_high*$prime_low) is $prod";
+echo "($prime_high**$prime_low) is $prod";
 
 ?>
