b02f841e8bb964b5f791a88611b64c672a628cea
SudeepMi <sudeepm0599@gmail.com>
2021-03-23 13:32:23 +0545

    task added for day 10 --- if else

--- Day 10/if/task4.php
+++ Day 10/if/task4.php
@@ -4,3 +4,7 @@ $x = 66;
 $y = 4;
 
 //perform modulus operation and check whether result is less than 5 or is even number
+
+// hint : use if statement and logical OR operator
+
+// if( condition1 || condition2)
---
+++ Day 9/increment decrement operator/task4.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = 98;
+
+
+// use pre increment to print supposed output
+
+
+
+
+echo $x; // output should be 100
